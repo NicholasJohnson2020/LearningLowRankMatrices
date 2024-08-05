3837227d5a7cb68a9f4271023261abb2b1b2330a
Nicholas Johnson <>
2024-08-04 21:09:45 -0400

    added r2 eval to synthetic data consolidation

--- scripts/consolidateData.jl
+++ scripts/consolidateData.jl
@@ -1,5 +1,23 @@
 using JSON, LinearAlgebra, Statistics, DataFrames, CSV
 
+function unserialize_matrix(mat)
+    """
+    This function unserializes an array of arrays into a 2-dimensional matrix.
+
+    :param mat: An array of arrays
+
+    :return: This function returns the matrix version of the input array of
+             arrays.
+    """
+    n = size(mat[1])[1]
+    m = size(mat)[1]
+    output = zeros(n, m)
+    for i=1:n, j=1:m
+        output[i, j] = mat[j][i]
+    end
+    return output
+end;
+
 function processData(input_path, method_name)
    """
    This function loads raw experiment output data as a JSON and processes it
@@ -19,7 +37,7 @@ function processData(input_path, method_name)
                   reconstruction_error_std=Float64[], objective=Float64[],
                   objective_std=Float64[], fitted_rank=Float64[],
                   fitted_rank_std=Float64[], exec_time=Float64[],
-                  exec_time_std=Float64[])
+                  exec_time_std=Float64[], r2=Float64[], r2_std=Float64[])
 
    successful_entries = 0
 
@@ -44,12 +62,15 @@ function processData(input_path, method_name)
       task_ID = file_name[end-6:end-5]
       if task_ID[1] == '_'
           task_ID = task_ID[2:end]
-      end;
+      end
 
       num_samples = length(exp_data["execution_time"])
       if num_samples == 0
          continue
       end
+
+      key = Dict{String, Real}(param_dict[string(task_ID)])
+      experiment_data = synthetic_data[string(key)]
 
       # Extract and store the relevant data
       current_row = [exp_data["N"],
@@ -69,6 +90,26 @@ function processData(input_path, method_name)
                      Statistics.mean(exp_data["execution_time"][2:end]),
                      Statistics.std(exp_data["execution_time"][2:end]) / (num_samples^0.5)]
 
+      r2_vals = []
+      for i=1:num_samples
+         X = unserialize_matrix(exp_data['solution'][i])
+         Y = unserialize_matrix(experiment_data[string(i)]["Y"])
+
+         beta = pinv(X'*X)*X'*Y
+         preds = X * beta
+         RSS = norm(Y - preds) ^ 2
+         mean_vec = mean(Y, dims=1)
+         TSS = 0
+         for i=1:size(Y)[1]
+             TSS += norm(Y[i, :] - mean_vec') ^ 2
+         end
+         r2 = 1 - (RSS / TSS)
+         append!(r2_vals, r2)
+      end
+
+      append!(current_row, Statistics.mean(r2_vals))
+      append!(current_row, Statistics.std(r2_vals) / (num_samples^0.5))
+
       push!(df, current_row)
       successful_entries += 1
 
@@ -82,6 +123,24 @@ METHOD_NAME = ARGS[1]
 INPUT_PATH = ARGS[2]
 
 OUTPUT_ROOT = INPUT_PATH * METHOD_NAME * "/" * METHOD_NAME
+
+# Load the syntehtic data
+synthetic_data = Dict()
+open(INPUT_PATH * "data.json", "r") do f
+    global synthetic_data
+    dicttxt = JSON.read(f, String)  # file information to string
+    synthetic_data = JSON.parse(dicttxt)  # parse and transform data
+    synthetic_data = JSON.parse(synthetic_data)
+end
+
+# Load the experiment parameters
+param_dict = Dict()
+open(INPUT_PATH * "params.json", "r") do f
+    global param_dict
+    dicttxt = JSON.read(f, String)  # file information to string
+    param_dict = JSON.parse(dicttxt)  # parse and transform data
+    param_dict = JSON.parse(param_dict)
+end
 
 # Process and save the data
 df1 = processData(INPUT_PATH, METHOD_NAME)
